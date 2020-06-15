106fb6280a58ff180cfc2c7d876ea968aac3a553
Willie Gomez <willie.gomezjr12@gmail.com>
2020-06-15 17:56:01 -0400

    update4

--- models/burger.js
+++ models/burger.js
@@ -6,7 +6,6 @@ var burger = {
         cb(res);
       });
     },
-    // The variables cols and vals are arrays.
     insertOne: function(cols, vals, cb) {
       orm.insertOne("burgers", cols, vals, function(res) {
         cb(res);
@@ -24,5 +23,4 @@ var burger = {
     }
   };
   
-  // Export the database functions for the controller
   module.exports = burger;
