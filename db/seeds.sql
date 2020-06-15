3db3092eb08da3caafd866cc2fc127e3b2960b7c
Willie Gomez <willie.gomezjr12@gmail.com>
2020-06-15 12:24:02 -0400

    updates

---
+++ db/seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO burgers (burger_name) VALUES ('Triple Cheeseburger'), ('Skinny Pounder Burger'), ('Supreme Bacon Burger');
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//Routes
+const routes = require("./controllers/burgers_controllers");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: http://localhost:" + PORT);
+});
